1e762e4edc07e10abd5ff574cbca8e0268bb47c1
sandeepraju241 <92543103+sandeepraju241@users.noreply.github.com>
2025-01-26 23:50:50 +0530

    Fix vite config and staticwebapp.json location

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
+  base: './', // Ensures correct asset paths for Azure
+  build: {
+    outDir: 'dist' // Ensures correct output folder
+  },
   plugins: [react()],
-
-})
+});
