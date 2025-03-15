b0be465a0fa7376c6ef05c3c13acc15ed8642962
sandeepraju241 <92543103+sandeepraju241@users.noreply.github.com>
2025-03-16 03:22:22 +0530

    build refresh fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
-// import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// // import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// // https://vite.dev/config/
+// // // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+
+// // })
+
+// import { defineConfig } from 'vite';
+// // import react from '@vitejs/plugin-react';
+
 // export default defineConfig({
 //   plugins: [react()],
-
-// })
+//   base: './', // This ensures paths are relative
+// });
 
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // This ensures paths are relative
+  base: '/', // Use absolute paths for assets
 });
